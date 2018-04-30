02feebe79da9a20044e9780899e85b459a501b7d
张双江 <zsjwish@126.com>
2018-04-30 11:32:59 +0800

    stack realize list

---
+++ src/leetcode/Lee232.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * created by zsj in 11:21 2018/4/30
+ * description:使用栈实现队列的下列操作：
+ * push(x) -- 将一个元素放入队列的尾部。
+ * pop() -- 从队列首部移除元素。
+ * peek() -- 返回队列首部的元素。
+ * empty() -- 返回队列是否为空。
+ **/
+public class Lee232 {
+
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+    /** Initialize your data structure here. */
+    public Lee232() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        stack2.push(x);
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return stack1.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        return stack1.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
