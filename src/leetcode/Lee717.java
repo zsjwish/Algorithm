0073c3f4344a794d54882088c725622298a95481
张双江 <zsjwish@126.com>
2018-04-30 14:36:14 +0800

    bits of 1 and 2

---
+++ src/leetcode/Lee717.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+/**
+ * created by zsj in 13:05 2018/4/30
+ * description:有两种特殊字符。第一种字符可以用一比特0来表示。第二种字符可以用两比特(10 或 11)来表示。
+ * 现给一个由若干比特组成的字符串。问最后一个字符是否必定为一个一比特字符。给定的字符串总是由0结束。
+ * 思路：首先判断长度是否是技术
+ **/
+public class Lee717 {
+
+    public static void main(String[] args) {
+        int[] bits = new int[] {1,1,1,1,0};
+        Lee717 lee717 = new Lee717();
+        lee717.isOneBitCharacter(bits);
+    }
+
+    private boolean isOneBitCharacter(int[] bits) {
+        if (bits == null || bits.length == 0) {
+            return false;
+        }
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i];
+        }
+        return i == bits.length-1;
+    }
+
+    public boolean isOneBitCharacter1(int[] bits) {
+        if (bits == null || bits.length == 0) {
+            return false;
+        }
+        if (bits.length == 1 & bits[0] == 0) {
+            return true;
+        }
+        int low = 0;
+        String tmp;
+        while (low < bits.length-2) {
+            tmp = "";
+            tmp = tmp + bits[low] + bits[low + 1];
+            if (tmp.equals("01")) {
+                low++;
+            }
+            else {
+                low = low + 2;
+            }
+        }
+        if (low == bits.length - 2) {
+            tmp = "";
+            tmp = tmp + bits[low] + bits[low + 1];
+            if (tmp.equals("00")) {
+                System.out.println("true");
+                return true;
+            }
+            System.out.println(false);
+            return false;
+        }
+        else if (low == bits.length - 1) {
+            if (bits[low] == 0) {
+                System.out.println(true);
+                return true;
+            }
+            System.out.println(false);
+            return false;
+        }
+        System.out.println("false s s ");
+        return false;
+    }
+}
