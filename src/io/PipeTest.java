76e82043d4b2d621fc7c2339143ef87f6a367906
张双江 <zsjwish@126.com>
2018-06-21 23:58:29 +0800

    exception test

--- src/io/PipeTest.java
+++ src/io/PipeTest.java
@@ -7,5 +7,15 @@ import java.io.PipedInputStream;
  * description:
  **/
 public class PipeTest {
-    PipedInputStream pi = null;
+    public static void main(String[] args) {
+        try {
+            throw new Exception("参数越界");
+        }catch (Exception e) {
+//            e.printStackTrace();
+//            System.out.println("1");
+//            e.printStackTrace();
+            e.printStackTrace();
+        }
+        System.out.println("2");
+    }
 }
